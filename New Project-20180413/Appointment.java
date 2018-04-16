36364f6ef538b76656d960ad16b15a8bc9a19a02
Gerardwww <31618081+Gerardwww@users.noreply.github.com>
2018-04-16 13:59:31 +0930

    Created by Coding Ground Client

---
+++ New Project-20180413/Appointment.java
@@ -0,0 +1,23 @@
+/*
+ * class Appointment
+ * 
+ * A representation of a Appointment for a patient attending the 
+ * Child Health Clinic.
+ * 
+ * You will need to implement the various elements of this class as desccribed
+ * in Stage 1 of the Assignment 1 Specification.
+ * 
+ */
+public class Appointment
+{
+
+   // write your code for the requirements described in Stage 1 here
+   // ...
+   
+   
+   
+   
+
+   
+
+}
